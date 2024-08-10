cf37dc0f16bbe172ae10d06852107e14dcdf9b83
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-08-10 17:19:28 -0400

    carousel aula sabado

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,28 @@
-import Header from "../componentes/Header"
+import Header from "../componentes/Header";
+import {Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Home(){
     return(
         <>
             <Header/>
-            <h1>home</h1>
+            <h1>Home</h1>
+            <Carousel
+                infiniteLoop
+                useKeyboardArrows
+                autoPlay
+                showArrows={true}
+                showStatus={false}
+                showThumbs={false}
+                dynamicHeight
+            >
+               <div>
+                    <img src=""/>
+               </div> 
+               <div>
+                    <img src=""/>
+               </div>     
+            </Carousel>
         </>      
     )
 }
