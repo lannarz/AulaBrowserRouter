6133416e03a734d79ff151bc312dc9bcef45532d
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-08-20 01:18:57 -0300

    carrinho e estrela

--- src/componentes/ListarProdutos.jsx
+++ src/componentes/ListarProdutos.jsx
@@ -11,7 +11,17 @@ export default function ListarProdutos( {produtos}){
                     <div className="separar">
                         <p className="titulo-produto">{obj.titulo}</p>
                         <p className="valor-produto">{obj.valor}</p>
+                        <img className="star" src="src/imagem/star.png"/>
                     </div>
+
+    
+
+                
+                    <div className="container">
+                        <div className="div1"><p className="arruma">Saiba mais</p></div>
+                        <div className="div2"><img className="carrinho" src="src/imagem/carrinho.png"/></div>
+                    </div>
+
                     </div>
                         
                     </div>
